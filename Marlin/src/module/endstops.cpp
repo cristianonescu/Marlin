1c48350703609d9e1ab1b29f04f6cc90f41dfd1d
Systemic <trash@laverriere.org>
2018-07-14 15:31:00 +0200

    Homing failed fix

--- Marlin/src/module/endstops.cpp
+++ Marlin/src/module/endstops.cpp
@@ -259,8 +259,8 @@ void Endstops::not_homing() {
 // If the last move failed to trigger an endstop, call kill
 void Endstops::validate_homing_move() {
   if (!trigger_state()) {
-    //kill(PSTR(MSG_ERR_HOMING_FAILED));
-    SERIAL_ECHOLNPGM("KILL HOMING MOVE");
+    kill(PSTR(MSG_ERR_HOMING_FAILED));
+    //SERIAL_ECHOLNPGM("KILL HOMING MOVE");
     }
   hit_on_purpose();
 }
